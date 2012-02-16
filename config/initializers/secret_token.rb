be21202cf66a1929a6485fce61d1b8f792c98f9d
Chap Ambrose <chapambrose@gmail.com>
2012-02-16 10:29:03 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Follower::Application.config.secret_token = '6f9939d7ff11f6bd37a6d4e60c76065c7b6d61b05fa3c72c3905085a72c37b31ee77059d3b409793870d8e8cd00f841503d99660d2ac6cc0ce813852328cefb2'
